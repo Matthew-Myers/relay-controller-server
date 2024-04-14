dd5c6826fa63c2abd5f08346568c26733f617e09
Matthew-Myers <matthew.myers.dev@gmail.com>
2024-04-14 17:16:46 -0400

    main

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use actix_web::{get, web, App, HttpServer, Responder};
 
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
-    let _ = blink()
-    format!("Hello {name}!")
+    let _ = blink();
+    format!("Hello {name}!");
 }
 
 use rppal::gpio::Gpio;
@@ -14,7 +14,6 @@ use rppal::system::DeviceInfo;
 
 // Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
 const GPIO_LED: u8 = 21;
-const mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
 
 
 #[actix_web::main] // or #[tokio::main]
@@ -29,6 +28,7 @@ async fn main() -> std::io::Result<()> {
 
 fn blink() -> Result<(), Box<dyn Error>> {
     println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
+    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
 
     // Blink the LED by setting the pin's logic level high for 500 ms.
     pin.set_high();
