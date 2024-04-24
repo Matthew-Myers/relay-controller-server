804c3fb1280cf4a44b81f8579bb7fb9b8d95f433
matt <myersmatt191@gmail.com>
2024-04-24 14:18:20 -0400

    adding in a couple endpoints plus middleware that I need to read and understand.  Middleware will set locks for the GPIO pins.

--- build-and-push.sh
+++ build-and-push.sh
@@ -12,4 +12,4 @@ readonly SOURCE_PATH=./target/${TARGET_ARCH}/release/relay-controller-server
 
 cargo build --release --target=${TARGET_ARCH}
 rsync ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
-ssh -t ${TARGET_HOST} ${TARGET_PATH}
+ssh -t ${TARGET_HOST} sudo ${TARGET_PATH}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,55 @@
-use std::error::Error;
+use actix_web::{
+    get, post, Error,
+    web::{self, Json, Path},
+    dev::{forward_ready, Service, ServiceRequest, ServiceResponse, Transform},
+    App, HttpResponse, HttpServer, Responder,
+};
+use std::future::{ready, Ready};
+use futures_util::future::LocalBoxFuture;
+
+use serde::{Deserialize, Serialize};
 use std::thread;
 use std::time::Duration;
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+use rppal::gpio::Gpio;
+use rppal::system::DeviceInfo;
 
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    let _ = blink();
+#[derive(Serialize, Deserialize)]
+struct LightToggleRequest {
+    pin: u8,
+    state: bool,
+}
+
+#[get("/test")]
+async fn test() -> impl Responder {
+    let _ = blink(21);
+    let _ = blink(20);
+    let _ = blink(16);
+    let _ = blink(12);
+    let _ = blink(25);
+    let _ = blink(24);
+    let _ = blink(23);
+    let _ = blink(18);
+
+    let _ = blink(21);
+    let _ = blink(20);
+    let _ = blink(16);
+    let _ = blink(12);
+    let _ = blink(25);
+    let _ = blink(24);
+    let _ = blink(23);
+    let _ = blink(18);
+
+    let _ = blink(21);
+    let _ = blink(20);
+    let _ = blink(16);
+    let _ = blink(12);
+    let _ = blink(25);
+    let _ = blink(24);
+    let _ = blink(23);
+    let _ = blink(18);
+
     HttpResponse::Ok().body("Hello world!")
 }
 
@@ -15,39 +58,123 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-async fn manual_hello() -> impl Responder {
+#[post("/light")]
+async fn light(req: Json<LightToggleRequest>) -> impl Responder {
+    println!("blinking on {}", req.pin);
+    let _ = blink(req.pin);
     HttpResponse::Ok().body("Hey there!")
 }
 
-use rppal::gpio::Gpio;
-use rppal::system::DeviceInfo;
+#[post("/blink/on")]
+async fn blinkon(req: Json<LightToggleRequest>) -> impl Responder {
+    println!("blinking on {}", req.pin);
+    let _ = blink_on(req.pin);
+    HttpResponse::Ok().body("Hey there!")
+}
 
-// Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
-const GPIO_LED: u8 = 21;
+#[post("/blink/off")]
+async fn blinkoff(req: Json<LightToggleRequest>) -> impl Responder {
+    println!("blinking on {}", req.pin);
+    let _ = blink_off(req.pin);
+    HttpResponse::Ok().body("Hey there!")
+}
 
+pub struct SayHi;
+// Middleware factory is `Transform` trait
+// `S` - type of the next service
+// `B` - type of response's body
+impl<S, B> Transform<S, ServiceRequest> for SayHi
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type InitError = ();
+    type Transform = SayHiMiddleware<S>;
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ready(Ok(SayHiMiddleware { service }))
+    }
+}
+pub struct SayHiMiddleware<S> {
+    service: S,
+}
+
+impl<S, B> Service<ServiceRequest> for SayHiMiddleware<S>
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Future = LocalBoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    forward_ready!(service);
+
+    fn call(&self, req: ServiceRequest) -> Self::Future {
+        println!("Hi from start. You requested: {}", req.path());
+        // Claim the locks required
+
+        let fut = self.service.call(req);
+
+        Box::pin(async move {
+            let res = fut.await?;
+            // release the locks
+            println!("Hi from response");
+            Ok(res)
+        })
+    }
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+        App::new()           
+            .wrap(SayHi)
+            .service(test)
+            .service(light)
+            .service(blinkoff)
+            .service(blinkon)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
 
-fn blink() -> Result<(), Box<dyn Error>> {
+fn blink(pin: u8) -> Result<(), Box<dyn std::error::Error>> {
     println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
-    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
+    let mut pin = Gpio::new().unwrap().get(pin).unwrap().into_output();
 
     // Blink the LED by setting the pin's logic level high for 500 ms.
     pin.set_high();
-    thread::sleep(Duration::from_millis(500));
+    thread::sleep(Duration::from_millis(250));
     pin.set_low();
 
     Ok(())
 }
 
+fn blink_on(pin: u8) -> Result<(), Box<dyn std::error::Error>> {
+    println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
+    let mut pin = Gpio::new().unwrap().get(pin).unwrap().into_output();
+
+    // Blink the LED by setting the pin's logic level high for 500 ms.
+    pin.set_high();
+    thread::sleep(Duration::from_millis(250));
+
+    Ok(())
+}
+
+fn blink_off(pin: u8) -> Result<(), Box<dyn std::error::Error>> {
+    println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
+    let mut pin = Gpio::new().unwrap().get(pin).unwrap().into_output();
+
+    // Blink the LED by setting the pin's logic level high for 500 ms.
+    pin.set_low();
+    thread::sleep(Duration::from_millis(250));
+
+    Ok(())
+}
